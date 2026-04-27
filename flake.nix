7931bf8c70a21ac376ebb271cf78b9bb9f673601
ahargunyllib <billy.bpm03@gmail.com>
2026-04-27 10:13:57 +0700

    chore: Add .gitignore and flake.nix for project setup

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "banking-peak-load-prototype's nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+  in
+  {
+    devShells."aarch64-darwin".default = pkgs.mkShell {
+      packages = [
+        pkgs.go
+      ];
+
+      shellHook = ''
+        export GOPATH="$HOME/go"
+        export GOBIN="$GOPATH/bin"
+        export PATH="$GOBIN:$PATH"
+      '';
+    };
+  };
+}
