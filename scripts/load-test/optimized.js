c64f3b088e9a7c81ee7cde860768e9e9a17edc80
Ega Yurcel Satriaji <egayurcel990@gmail.com>
2026-05-06 17:32:29 +0700

    setup kubernetes orchestration and ci/cd pipeline

--- scripts/load-test/optimized.js
+++ scripts/load-test/optimized.js
@@ -78,7 +78,7 @@ function buildTransactionPayload() {
 // Main Test
 // =========================
 export default function () {
-  const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
+  const BASE_URL = __ENV.BASE_URL || 'http://127.0.0.1:35279';
 
   const payload = buildTransactionPayload();
 
